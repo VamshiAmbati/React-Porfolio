c7a62a01c075182d2e55b279677fe828fb09bc02
VamshiAmbati <vamshiambati27@gmail.com>
2024-09-08 18:32:35 +0530

    fifth commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default {
-  build: {
-    rollupOptions: {
-      external: ["react-icons/lu"],
-    },
-  },
-};
+export default defineConfig({
+  plugins: [react()],
+});
